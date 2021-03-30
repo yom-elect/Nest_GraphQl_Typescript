0289267b927594ea23f2b95ce89024087b4956ab
yomi <adepitanoriyomi11@yahoo.com>
2021-03-30 18:59:01 +0100

    project architecture

---
+++ src/users/dto/input/create-user.input.ts
@@ -0,0 +1,15 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+  
+  @Field()
+  @IsNotEmpty()
+  age: number;
+}
